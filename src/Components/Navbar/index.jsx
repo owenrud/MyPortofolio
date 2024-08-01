f68083f7ef4b2edb955040e6ce48957ada504c65
owenrud <owenrudiantoso@gmail.com>
2024-08-01 19:51:40 +0700

    fix bug

--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -3,10 +3,10 @@ const Navbar = () =>{
     return(
         <nav>
         <div className=" flex md:flex-row flex-col justify-center items-center mb-2 ">
-        <Link href="#home"  className="md:self-center text-xl font-bold whitespace-nowrap text-color-primary dark:text-color-primary p-4 hover:text-blue-800">Home</Link>
-        <Link href="#about" className="md:self-center text-xl font-bold whitespace-nowrap text-color-primary p-4 hover:text-blue-800">About</Link>
-        <Link href="/" className="md:self-center text-xl font-bold whitespace-nowrap text-color-primary p-4 hover:text-blue-800">Portofolio</Link>
-        <Link href="/" className="md:self-center text-xl font-bold whitespace-nowrap text-color-primary p-4 hover:text-blue-800">Contact</Link>
+        <Link href="#home"  className="text-xl font-bold whitespace-nowrap text-color-primary dark:text-color-primary p-4 hover:text-blue-800">Home</Link>
+        <Link href="#about" className="text-xl font-bold whitespace-nowrap text-color-primary p-4 hover:text-blue-800">About</Link>
+        <Link href="/" className="text-xl font-bold whitespace-nowrap text-color-primary p-4 hover:text-blue-800">Portofolio</Link>
+        <Link href="/" className="text-xl font-bold whitespace-nowrap text-color-primary p-4 hover:text-blue-800">Contact</Link>
         </div>
         </nav>
     )
