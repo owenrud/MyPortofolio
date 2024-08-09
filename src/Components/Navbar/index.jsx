e8f2c2ca15f6afcdeaec562826b526203b330c01
owenrud <owenrudiantoso@gmail.com>
2024-08-10 00:16:43 +0800

    update about me

--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -3,7 +3,7 @@ import Menu from "./menu"
 const Navbar = () =>{
     
     return(
-        <nav className="bg-color-primary text-color-accent border border-color-secondary shadow-xl">
+        <nav className="bg-color-primary text-color-accent border border-color-gray shadow-xl">
         <div className="flex flex-col space-y-2 md:flex-row justify-evenly items-center mb-2 p-2">
             <Link href="/" className="flex-1 max-w-64 max-h-12 text-lg md:text-2xl bg-gradient-to-br font-bold from-color-accent to-color-secondaryAccent inline-block text-transparent bg-clip-text">
                 Owen Rudiyanto So
--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,14 +1,14 @@
 import Image from "next/image";
 import srcImage from "@/app/img_about.jpg";
-import { Progress } from "flowbite-react";
+
 const Page = () => {
   return (
     <>
-      <div className="flex-1 flex-col justify-center items-center bg-color-accent pt-4">
-        <h1 className="text-2xl md:text-4xl p-2 font-bold text-color-primary pt-16 pb-8 mt-8 text-center">
+      <div className="flex-1 flex-col justify-center items-center bg-color-primary min-h-screen pt-4">
+        <h1 className="text-2xl md:text-4xl p-2 font-bold text-color-secondary pt-16 pb-8 mt-8 text-center">
           About Me
         </h1>
-        <div className="flex flex-col justify-center items-center md:flex-row bg-color-primary p-4 md:p-12 rounded-lg space-x-4 space-y-4">
+        <div className="flex flex-col justify-center items-center md:flex-row bg-gradient-to-br from-color-gray  to-color-secondaryAccent p-4 md:p-12 rounded-lg space-x-4 space-y-4">
           <Image
             id="menu1"
             className="rounded-xl"
@@ -23,9 +23,8 @@ const Page = () => {
             width={200}
             height={200}
           ></Image>
-          <div className="flex flex-col">
-
-            <h2 className="text-xl p-2 text-justify text-color-accent">
+          <div className="flex flex-col p-4 space-y-2">
+            <h2 className="text-xl  text-justify text-color-accent mb-4">
               I’m a Person That Always Combine My Great Time Management & Typing
               Speed to Always Fulfill My Tasks Before Deadline. I’m Also a
               Person That Have Strong Problem Solving & Logic Skills, Great
@@ -34,20 +33,107 @@ const Page = () => {
               Making Me Comfortable When Talking. I’m Attracted in the field of
               IT Software & Hardware, especially Programming World.
             </h2>
+            <div className="mb-1 text-xl font-medium text-color-secondary md:text-left text-center">
+              Skills
+            </div>
+            <div className="flex md:flex-row space-x-16">
+              <div className="flex-1 md:flex-col space-y-4">
 
-            <Progress
-          progress={45}
-          progressLabelPosition="inside"
-          textLabel="Flowbite"
-          textLabelPosition="outside"
-          size="lg"
-          labelProgress
-          labelText
-            />
+                <div class="flex justify-between mb-1">
+                  <span class="text-xs md:text-base font-medium text-color-accent dark:text-white">
+                    HTML & CSS & JS
+                  </span>
+                  <span class="text-sm font-medium text-color-accent dark:text-white">
+                    83%
+                  </span>
+                </div>
+                <div class="w-full bg-gray-200 rounded-full h-2.5 dark:bg-gray-700">
+                  <div
+                    class="bg-gradient-to-r from-color-secondary to-color-accent h-2.5 rounded-full"
+                    style={{ width: "83%" }}
+                  ></div>
+                </div>
+
+                <div class="flex justify-between mb-1">
+                  <span class="text-base font-medium text-color-accent dark:text-white">
+                    PHP
+                  </span>
+                  <span class="text-sm font-medium text-color-accent dark:text-white">
+                    90%
+                  </span>
+                </div>
+                <div class="w-full bg-gray-200 rounded-full h-2.5 dark:bg-gray-700">
+                  <div
+                    class="bg-gradient-to-r from-color-secondary to-color-accent h-2.5 rounded-full"
+                    style={{ width: "90%" }}
+                  ></div>
+                </div>
+
+                <div class="flex justify-between mb-1">
+                  <span class="text-base font-medium text-color-accent dark:text-white">
+                    Next.JS
+                  </span>
+                  <span class="text-sm font-medium text-color-accent dark:text-white">
+                    40%
+                  </span>
+                </div>
+                <div class="w-full bg-gray-200 rounded-full h-2.5 dark:bg-gray-700">
+                  <div
+                    class="bg-gradient-to-r from-color-secondary to-color-accent h-2.5 rounded-full"
+                    style={{ width: "40%" }}
+                  ></div>
+                </div>
+
+              </div>
+
+              <div className="flex-1 md:flex-col space-y-4">
+              <div class="flex justify-between mb-1">
+                  <span class="text-base font-medium text-color-accent dark:text-white">
+                    MySQL
+                  </span>
+                  <span class="text-sm font-medium text-color-accent dark:text-white">
+                    90%
+                  </span>
+                </div>
+                <div class="w-full bg-gray-200 rounded-full h-2.5 dark:bg-gray-700">
+                  <div
+                    class="bg-gradient-to-r from-color-secondary to-color-accent h-2.5 rounded-full"
+                    style={{ width: "90%" }}
+                  ></div>
+                </div>
+
+                <div class="flex justify-between mb-1">
+                  <span class="text-base font-medium text-color-accent dark:text-white">
+                    Flutter
+                  </span>
+                  <span class="text-sm font-medium text-color-accent dark:text-white">
+                    50%
+                  </span>
+                </div>
+                <div class="w-full bg-gray-200 rounded-full h-2.5 dark:bg-gray-700">
+                  <div
+                    class="bg-gradient-to-r from-color-secondary to-color-accent h-2.5 rounded-full"
+                    style={{ width: "50%" }}
+                  ></div>
+                </div>
+                <div class="flex justify-between mb-1">
+                  <span class="text-base font-medium text-color-accent dark:text-white">
+                    Java
+                  </span>
+                  <span class="text-sm font-medium text-color-accent dark:text-white">
+                    30%
+                  </span>
+                </div>
+                <div class="w-full bg-gray-200 rounded-full h-2.5 dark:bg-gray-700">
+                  <div
+                    class="bg-gradient-to-r from-color-secondary to-color-accent h-2.5 rounded-full"
+                    style={{ width: "30%" }}
+                  ></div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
-
-        
       </div>
     </>
   );
