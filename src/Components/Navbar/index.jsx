91010cb73f08f6b9268bb5129a935671c8bbb226
owenrud <owenrudiantoso@gmail.com>
2024-08-09 21:10:56 +0800

    small fix

--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -4,7 +4,7 @@ const Navbar = () =>{
     
     return(
         <nav className="bg-color-primary text-color-accent border border-color-secondary shadow-xl">
-        <div className="flex flex-col space-y-2 md:flex-row justify-evenly sm:space-y-2 items-center mb-2 p-2">
+        <div className="flex flex-col space-y-2 md:flex-row justify-evenly items-center mb-2 p-2">
             <div className="flex-1 max-w-64 max-h-12 text-lg md:text-2xl font-bold text-color-secondaryAccent">
                 Owen Rudiyanto So
             </div>
--- src/Components/Navbar/menu.jsx
+++ src/Components/Navbar/menu.jsx
@@ -4,7 +4,7 @@ import Link from "next/link"
 const Menu = () =>{
     return(
         <>
-        <div  className="flex flex-row gap-16">
+        <div  className="flex flex-row gap-8">
             <Link href="/"  className="text-sm md:text-2xl font-bold whitespace-nowrap  hover:text-color-secondaryAccent"><House size={24} /></Link>
             <Link href="/about" className="text-sm md:text-2xl font-bold whitespace-nowrap hover:text-color-secondaryAccent"><UserRectangle size={24} /></Link>
             <Link href="/" className="text-sm md:text-2xl font-bold whitespace-nowrap hover:text-color-secondaryAccent"><ClipboardText size={24} /></Link>
