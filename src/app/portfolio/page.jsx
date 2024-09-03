648c3c2d4ceabcdb1a2c3a1bd696125b2147fbbe
owenrud <owenrudiantoso@gmail.com>
2024-09-04 02:57:34 +0800

    update portfolio details

--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -1,65 +1,157 @@
+"use client";
 
-import { Carousel } from "flowbite-react";
-import img1 from "@/app/portfolio-1.png"
-import img2 from "@/app/portfolio-2.png"
-import img3 from "@/app/portfolio-3.png"
-import img4 from "@/app/portfolio-4.png"
-import img5 from "@/app/portfolio-5.png"
-import img6 from "@/app/portfolio-6.png"
-import img7 from "@/app/portfolio-7.png"
+import { Button, Tabs, TabsRef } from "flowbite-react";
+import { useRef, useState } from "react";
+import img1 from "@/app/portfolio-1.png";
+import img2 from "@/app/portfolio-2.png";
+import img3 from "@/app/portfolio-3.png";
+import img4 from "@/app/portfolio-4.png";
+import img5 from "@/app/portfolio-5.png";
+import img6 from "@/app/portfolio-6.png";
+import img7 from "@/app/portfolio-7.png";
 import Image from "next/image";
 
-const Page = () => {
-    return(
-        <>
-        <div className="h-screen p-8 bg-gradient-to-br from-color-winterGradient to-color-secondary overflow-auto">
-            <h1 className=" text-3xl text-center text-color-winter pt-20 xl:pt-20 md:pl-4 min-[1921px]:pt-20 min-[1921px]:text-5xl">My Portfolio</h1>
-            <div className="grid grid-cols-1 md:grid-cols-3 gap-8 justify-center items-center mt-8 mx-8">
-                <div className="relative flex flex-col">
-                <Image className=" mx-auto" src={img1} width={1000} height={1000} style={{objectFit: "scale-down"}}></Image>
-                <h1 className="absolute ContentImg flex  items-center justify-center text-xl md:text-4xl font-semibold text-center text-color-winterGradient hover:cursor-pointer">Shuttle Bus Website</h1>
-                <h1 className="ContentImgMobile px-4 py-1.5 bg-color-gray bg-opacity-75 text-base md:text-4xl font-semibold text-center text-color-winterGradient hover:cursor-pointer">Shuttle Bus Website</h1>
-                </div>
-                <div className="relative flex flex-col">
-                <Image  className="mx-auto" src={img2} width={1000} height={1000} style={{objectFit: "scale-down"}}></Image>
-                <h1 className="absolute ContentImg flex bg-color-secondary items-center justify-center text-xl md:text-4xl font-semibold text-center text-color-winterGradient hover:cursor-pointer">Volunteer Recruitment Website</h1>
-                <h1 className="ContentImgMobile px-4 py-1.5  bg-color-gray bg-opacity-75 text-base md:text-4xl font-semibold text-center text-color-winterGradient hover:cursor-pointer">Volunteer Recruitment Website</h1>
-                </div>
-                <div className="relative flex flex-col">
-                <Image className="mx-auto" src={img3} width={1000} height={1000} style={{objectFit: "scale-down"}}></Image>
-                <h1 className="absolute ContentImg flex bg-color-secondary items-center justify-center text-xl md:text-4xl font-semibold text-center text-color-winterGradient hover:cursor-pointer">Prototype Game</h1>
-                <h1 className="ContentImgMobile px-4 py-1.5 bg-color-gray bg-opacity-75 text-base md:text-4xl font-semibold text-center text-color-winterGradient hover:cursor-pointer">Prototype Game</h1>
-                </div>
-                <div className="relative flex flex-col">
-                <Image  className="mx-auto" src={img4} width={1000} height={1000} style={{objectFit: "scale-down"}}></Image>
-                <h1 className="absolute ContentImg flex bg-color-secondary items-center justify-center text-xl md:text-4xl font-semibold text-center text-color-winterGradient hover:cursor-pointer">Company Profile</h1>
-                <h1 className="ContentImgMobile px-4 py-1.5 bg-color-gray bg-opacity-75 text-base md:text-4xl font-semibold text-center text-color-winterGradient hover:cursor-pointer">Company Profile</h1>
-                </div>
-                <div className="relative flex flex-col">
-                <Image  className="mx-auto" src={img5} width={1000} height={1000} style={{objectFit: "scale-down"}}></Image>
-                <h1 className="absolute ContentImg flex bg-color-secondary items-center justify-center text-xl md:text-4xl font-semibold text-center text-color-winterGradient hover:cursor-pointer">Anime List</h1>
-                <h1 className="ContentImgMobile px-4 py-1.5 bg-color-gray bg-opacity-75 text-base md:text-4xl font-semibold text-center text-color-winterGradient hover:cursor-pointer">Anime List</h1>
-                </div>
-                <div className="relative flex flex-col">
-                <Image  className="mx-auto" src={img6} width={1000} height={1000} style={{objectFit: "scale-down"}}></Image>
-                <h1 className="absolute ContentImg flex bg-color-secondary items-center justify-center text-xl md:text-4xl font-semibold text-center text-color-winterGradient hover:cursor-pointer">System Registration Digital Event</h1>
-                <h1 className="ContentImgMobile px-4 py-1.5 bg-color-gray bg-opacity-75 text-base md:text-4xl font-semibold text-center text-color-winterGradient hover:cursor-pointer">System Registration Digital Event</h1>
-                </div>
-                <div className="relative flex flex-col">
-                <Image  className="mx-auto" src={img7} width={1000} height={1000} style={{objectFit: "scale-down"}}></Image>
-                <h1 className="absolute ContentImg flex bg-color-secondary items-center justify-center text-xl md:text-4xl font-semibold text-center text-color-winterGradient hover:cursor-pointer">Health & Clinic Website</h1>
-                <h1 className="ContentImgMobile px-4 py-1.5 bg-color-gray bg-opacity-75 text-base md:text-4xl font-semibold text-center text-color-winterGradient hover:cursor-pointer">Health & Clinic Website</h1>
-                </div>
-        
-       
-       
-       
-       
-        
-      </div>
-    </div>
-        </>
-    )
-}
+const tabsContent = {
+  All: [
+    {
+      title: "Shuttle Bus Website",
+      image: img1,
+      description: "Shuttle Bus Website",
+      category: "Next.js",
+    },
+    {
+      title: "Volunteer Recruitment Website",
+      image: img2,
+      description: "Volunteer Recruitment Website",
+      category: "Laravel",
+    },
+    {
+      title: "Prototype Game",
+      image: img3,
+      description: "Prototype Game",
+      category: "Next.js",
+    },
+    {
+      title: "Company Profile",
+      image: img4,
+      description: "Company Profile",
+      category: "Laravel",
+    },
+    {
+      title: "Anime List",
+      image: img5,
+      description: "Anime List",
+      category: "Next.js",
+    },
+    {
+      title: "System Registration Digital Event",
+      image: img6,
+      description: "System Registration Digital Event",
+      category: "Laravel",
+    },
+    {
+      title: "Health & Clinic Website",
+      image: img7,
+      description: "Health & Clinic Website",
+      category: "Next.js",
+    },
+  ],
+  Laravel: [
+    {
+      title: "Volunteer Recruitment Website",
+      image: img2,
+      description: "Volunteer Recruitment Website",
+      category: "Laravel",
+    },
+    {
+      title: "Company Profile",
+      image: img4,
+      description: "Company Profile",
+      category: "Laravel",
+    },
+    {
+      title: "System Registration Digital Event",
+      image: img6,
+      description: "System Registration Digital Event",
+      category: "Laravel",
+    },
+  ],
+  "Next.js": [
+    {
+      title: "Shuttle Bus Website",
+      image: img1,
+      description: "Shuttle Bus Website",
+      category: "Next.js",
+    },
 
-export default Page
+    {
+      title: "Anime List",
+      image: img5,
+      description: "Anime List",
+      category: "Next.js",
+    },
+    {
+      title: "Health & Clinic Website",
+      image: img7,
+      description: "Health & Clinic Website",
+      category: "Next.js",
+    },
+  ],
+  "Game Development": [
+    {
+      title: "Prototype Game",
+      image: img3,
+      description: "Prototype Game",
+      category: "Next.js",
+    },
+  ],
+};
+const Page = () => {
+  const [activeTab, setActiveTab] = useState("All");
+
+  return (
+    <>
+      <div className="h-screen p-8 bg-gradient-to-br from-color-winterGradient to-color-secondary overflow-auto">
+        <h1 className="text-3xl text-center text-white py-10">My Portfolio</h1>
+
+        <div className="mb-4">
+          <div className="grid grid-cols-2 md:grid-cols-4 gap-4 items-center justify-center">
+            {Object.keys(tabsContent).map((tab) => (
+              <button
+                key={tab}
+                onClick={() => setActiveTab(tab)}
+                className={`px-4 py-2 rounded-lg text-sm md:text-lg font-semibold transition-colors md:mx-16 ${
+                  activeTab === tab
+                    ? "bg-color-winter text-color-secondary"
+                    : "bg-color-winterGradient text-color-winter"
+                }`}
+              >
+                {tab}
+              </button>
+            ))}
+          </div>
+        </div>
+
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 mt-16">
+          {tabsContent[activeTab].map((item, index) => (
+            <div key={index} className="relative flex flex-col justify-center items-center">
+              <Image
+                className="rounded-lg"
+                src={item.image}
+                width={1000}
+                height={1000}
+                
+                alt={item.title}
+              />
+              <h1 className="absolute flex bottom-0 left-1/2 transform -translate-x-1/2 px-4 py-2 bg-color-winterSecondaryGradient bg-opacity-75 text-color-winter text-lg md:text-2xl font-semibold text-center justify-center  items-center rounded w-full h-10 md:h-16">
+                {item.description}
+              </h1>
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Page;
